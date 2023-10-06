c358ddfae3a93c658cfd6516871500738278c9a3
developerrayhan <developerrayhan15@gmail.com>
2023-10-07 04:34:40 +0600

    complete header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -9,11 +9,13 @@ import { PiInstagramLogoFill } from "react-icons/pi";
 import { BsTwitter } from "react-icons/bs";
 import { RxMagnifyingGlass } from "react-icons/rx";
 import { IoMdMail } from "react-icons/io";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { IoCloseOutline } from "react-icons/io5";
 
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
-    weight: ["500"],
+    weight: ["400", "500"],
     subsets: ["latin"],
 });
 
@@ -22,6 +24,15 @@ const Header = () => {
     const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
     const searchBarRef = useRef(null);
     const [isHeaderFixed, setIsHeaderFixed] = useState(false);
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const handleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    const handleMobileMenuClose = () => {
+        setIsMobileMenuOpen(false);
+    };
 
     const handleSearch = () => {
         setIsSearchBarOpen(!isSearchBarOpen);
@@ -59,11 +70,38 @@ const Header = () => {
         };
     }, [searchBarRef]);
 
+    const NavItems = [
+        {
+            name: "Home",
+            link: "/",
+        },
+        {
+            name: "About us",
+            link: "/about",
+        },
+        {
+            name: "Projects",
+            link: "/projects",
+        },
+        {
+            name: "Services",
+            link: "/services",
+        },
+        {
+            name: "Blog",
+            link: "/blog",
+        },
+        {
+            name: "Contact us",
+            link: "/contact",
+        },
+    ];
+
     return (
-        <header className="">
+        <header className="relative">
             <div
                 className={`
-                ${isHeaderFixed ? "hidden" : "container top-logo flex items-center justify-center h-[80px]"}
+                ${isHeaderFixed ? "hidden" : "container top-logo hidden md:flex items-center justify-center h-[80px]"}
             `}
             >
                 <Link href="/" className="py-[1.37rem]">
@@ -71,15 +109,23 @@ const Header = () => {
                 </Link>
             </div>
             <nav
-                className={`flex items-center justify-between px-[1.87rem] h-[80px]
+                className={`flex items-center justify-between px-[1.87rem] h-[60px] lg:h-[80px]
                 ${
                     isHeaderFixed
-                        ? "fixed top-0 left-0 right-0 z-[999] bg-[#282d32] shadow-lg flex-row-reverse slide-down"
+                        ? "fixed top-0 left-0 right-0 z-[999] bg-[#282d32] shadow-lg md:flex-row-reverse slide-down"
                         : "container"
                 }
             `}
             >
-                <ul className="flex items-center gap-[1.44rem]">
+                <Link
+                    href="/"
+                    className={`
+                    ${isHeaderFixed ? "hidden" : "py-[1.37rem] md:hidden"}
+                `}
+                >
+                    <Image src={StickyLogo} alt="logo" width={300} height={60} className="w-[200px] h-[60px]" />
+                </Link>
+                <ul className="hidden lg:flex items-center gap-[1.44rem]">
                     <li>
                         <Link href={"#"}>
                             <FaFacebookF size={20} className="text-white" />
@@ -101,77 +147,50 @@ const Header = () => {
                         </Link>
                     </li>
                 </ul>
-                <ul className="flex items-center gap-[1.81rem]">
-                    <li>
-                        <Link
-                            href="/"
-                            className={`font-medium leading-[187.5%] text-base uppercase tracking-[0.02rem] inline-block relative py-[30px] transition-all duration-300 hover:text-[--main-color] ${
-                                currentRoute === "/" ? "text-[--main-color]" : "text-white"
-                            }`}
-                            style={poppins.style}
-                        >
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="/about"
-                            className={`font-medium leading-[187.5%] text-base uppercase tracking-[0.02rem] inline-block relative py-[30px] transition-all duration-300 hover:text-[--main-color] ${
-                                currentRoute === "/about" ? "text-[--main-color]" : "text-white"
-                            }`}
-                            style={poppins.style}
-                        >
-                            About us
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="/projects"
-                            className={`font-medium leading-[187.5%] text-base uppercase tracking-[0.02rem] inline-block relative py-[30px] transition-all duration-300 hover:text-[--main-color] ${
-                                currentRoute === "/projects" ? "text-[--main-color]" : "text-white"
-                            }`}
-                            style={poppins.style}
-                        >
-                            Projects
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="/services"
-                            className={`font-medium leading-[187.5%] text-base uppercase tracking-[0.02rem] inline-block relative py-[30px] transition-all duration-300 hover:text-[--main-color] ${
-                                currentRoute === "/services" ? "text-[--main-color]" : "text-white"
-                            }`}
-                            style={poppins.style}
-                        >
-                            Services
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="/blog"
-                            className={`font-medium leading-[187.5%] text-base uppercase tracking-[0.02rem] inline-block relative py-[30px] transition-all duration-300 hover:text-[--main-color] ${
-                                currentRoute === "/blog" ? "text-[--main-color]" : "text-white"
-                            }`}
-                            style={poppins.style}
-                        >
-                            Blog
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="/contact"
-                            className={`font-medium leading-[187.5%] text-base uppercase tracking-[0.02rem] inline-block relative py-[30px] transition-all duration-300 hover:text-[--main-color] ${
-                                currentRoute === "/contact" ? "text-[--main-color]" : "text-white"
-                            }`}
-                            style={poppins.style}
-                        >
-                            Contact us
-                        </Link>
-                    </li>
+                {/* Desktop menu */}
+                <ul
+                    className={` hidden md:flex items-center gap-[1.81rem]
+                    ${isHeaderFixed ? "" : "md:mx-auto"}
+                `}
+                >
+                    {NavItems.map((item, index) => (
+                        <li key={index}>
+                            <Link
+                                href={item.link}
+                                className={`font-medium leading-[187.5%] text-base uppercase tracking-[0.02rem] inline-block relative py-[30px] transition-all duration-300 hover:text-[--main-color] ${
+                                    currentRoute === item.link ? "text-[--main-color]" : "text-white"
+                                }`}
+                            >
+                                {item.name}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
+                {/* mobile menu */}
+                <div
+                    className={`absolute top-full left-0 bg-[--secondary-color] h-screen w-[340px] transform transition-transform duration-500 ease-in-out z-[999] p-0 inline-block md:hidden ${
+                        isMobileMenuOpen ? "translate-x-0" : "-translate-x-full"
+                    }`}
+                >
+                    <ul>
+                        {NavItems.map((item, index) => (
+                            <li key={index}>
+                                <Link
+                                    href={item.link}
+                                    className={`font-medium leading-[187.5%] text-base uppercase tracking-[0.02rem] inline-block relative px-[1.87rem] py-[10px] transition-all duration-300 hover:text-[--main-color] ${
+                                        currentRoute === item.link ? "text-[--main-color]" : "text-white"
+                                    }`}
+                                >
+                                    {item.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                {/* icon set 2 */}
                 <ul
                     className={`
-                    ${isHeaderFixed ? "hidden" : "flex items-center gap-[1.81rem]"}
+                    ${isHeaderFixed ? "hidden" : "hidden lg:flex items-center gap-[1.81rem]"}
                 `}
                 >
                     <li>
@@ -185,6 +204,7 @@ const Header = () => {
                         </Link>
                     </li>
                 </ul>
+                {/* sticky logo */}
                 <Link
                     href="/"
                     className={`
@@ -193,6 +213,7 @@ const Header = () => {
                 >
                     <Image src={StickyLogo} alt="logo" width={300} height={80} className="w-[200px] h-[80px]" />
                 </Link>
+                {/* search bar */}
                 <div
                     className={`fixed bg-[#282d32]/90 inset-0 transition-all transform ease-in-out delay-400 duration-500 h-screen flex items-center justify-center ${
                         isSearchBarOpen ? "translate-y-0 z-[999]" : "-translate-y-[100%]"
@@ -227,7 +248,16 @@ const Header = () => {
                         />
                     </div>
                 </div>
+                {/* toggle button */}
+                <button className="inline-flex md:hidden">
+                    {isMobileMenuOpen ? (
+                        <IoCloseOutline size={30} className="text-white" onClick={handleMobileMenuClose} />
+                    ) : (
+                        <HiMenuAlt4 size={30} className="text-white" onClick={handleMobileMenu} />
+                    )}
+                </button>
             </nav>
+            {/* mobile menu */}
         </header>
     );
 };
